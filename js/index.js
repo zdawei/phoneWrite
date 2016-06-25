66f7e18bc6ef4f72ef4738bc0dd2a6be94fdc116
dawei <weiweicat333@gmail.com>
2016-06-26 02:00:25 +0800

    updata

--- js/index.js
+++ js/index.js
@@ -7,105 +7,83 @@ var W = function(){
   canvas.id = "writing";
   document.body.insertBefore(canvas,document.body.lastChild);
   var ctx = canvas.getContext("2d");
-  var currentChar = 0,totalchar = 15;
+  var currentChar = 0,totalchar = 15;//currentChar 当前的汉子个数,totalchar总汉子个数
   var countChar = document.getElementById("tip").firstChild.firstChild;//提示的文本
   var canvasURLArray = [],charDatas = [];//charDatas 写汉字的保存数组
   var $ = {
-  	x = [],//x坐标
-  	y = [],//y坐标
-  	time = [],//时间
-    count = 0,//数组索引
-    locks = [],//汉字锁数组，用来防止笔画之间的链接,还可以用来计算出笔画数
-  	speed = [],//速度
-  	pressure = [],//压力
-  	distance = [],//距离
+  	x : [],//x坐标
+  	y : [],//y坐标
+  	time : [],//时间
+    count : 0,//数组索引
+    locks : [],//汉字锁数组，用来防止笔画之间的链接,还可以用来计算出笔画数
+  	speed : [],//速度
+  	pressure : [],//压力
+  	distance : [],//距离
   	//以下是参数设置
-  	gaoss = 1.3,//高斯初始值
-  	minPress = 0.05,
-  	maxPress = 0.2,
-  	width = 50
+  	gaoss : 1.3,//高斯初始值
+  	minPress : 0.05,
+  	maxPress : 0.2,
+  	width : 50
   };
 
-  var gaussian = function(v, gauss) {//高斯计算公式
-		return ((1 / (Math.sqrt(2 * Math.PI) * gauss)) * Math.pow(Math.E,-(v * v) / (2 * gauss * gauss)));
-	}
-	var parameter = function(){
-		var ratio = $.maxPress / gaussian(0,$.gaoss);
-		var timeGap = $.time[$.count - 1] - $.time[$.count - 2];
-		var distance = Math.sqrt(Math.pow($.x[$.count - 1] - $.x[$.count - 2],2) +
-			Math.pow($.y[$.count - 1] - $.y[$.count - 2],2));
-		$.distance.push(distance);
-		if(timeGap == 0){
-			$.speed[$.count - 1] = $.speed[$.count - 2];
-		}else{
-			$.speed[$.count - 1] = distance / timeGap;
-		}
-		if($.count > 2){
-			$.speed[$.count - 1] = $.speed[$.count - 1] * 0.6 + $.speed[$.count - 2] * 0.3 + $.speed[$.count - 3] * 0.1;
-		}
-		var speed = gaussian($.speed[$.count - 1],$.gaoss);
-		$.pressure[$.count - 1] = (ratio * speed);
-    $.pressure[$.count - 1] = Math.max(Math.min($.pressure[$.count - 1],$.maxPress),$.minPress);
-		$.pressure[$.count - 1] = ($.pressure[$.count - 1] + $.pressure[$.count - 2]) / 2;
-	}
-
-  function dl(context,x1,y1,x2,y2,dashLength){
-  	dashLength=dashLength===undefined?5:dashLength;
-  	var deltaX=x2-x1;
-  	var deltaY=y2-y1;
-  	var numDashes=Math.floor(Math.sqrt(deltaX*deltaX+deltaY*deltaY)/dashLength);
-  	for(var i=0;i<numDashes;++i){
-  		context[i%2===0?'moveTo':'lineTo']
-  		(x1+(deltaX/numDashes)*i,y1+(deltaY/numDashes)*i);
-  	}
-  	context.stroke();
+  var qt = function(ctx){
+    var dl = function(context,x1,y1,x2,y2,dashLength){
+      dashLength=dashLength===undefined?5:dashLength;
+      var deltaX=x2-x1;
+      var deltaY=y2-y1;
+      var numDashes=Math.floor(Math.sqrt(deltaX*deltaX+deltaY*deltaY)/dashLength);
+      for(var i=0;i<numDashes;++i){
+        context[i%2===0?'moveTo':'lineTo']
+        (x1+(deltaX/numDashes)*i,y1+(deltaY/numDashes)*i);
+      }
+      context.stroke();
+    }
+    ctx.beginPath();
+    ctx.strokeStyle='black';
+    ctx.lineWidth=1.5;
+    ctx.moveTo(0,ctx.canvas.height/2);
+    ctx.lineTo(ctx.canvas.width,ctx.canvas.height/2);
+    ctx.moveTo(ctx.canvas.width/2,0);
+    ctx.lineTo(ctx.canvas.width/2,ctx.canvas.height);
+    ctx.stroke();
+    ctx.lineWidth=0.5;
+    dl(ctx,0,0,ctx.canvas.width,ctx.canvas.height,10);
+    dl(ctx,0,ctx.canvas.height,ctx.canvas.width,0,10);
+    ctx.closePath();
   }
+  //米字格以上 qt=======直接用qt(ctx)即可画出米字格
 
-  function qt(ctx){
-  ctx.beginPath();
-  ctx.strokeStyle='black';
-  ctx.lineWidth=1.5;
-  ctx.moveTo(0,ctx.canvas.height/2);
-  ctx.lineTo(ctx.canvas.width,ctx.canvas.height/2);
-  ctx.moveTo(ctx.canvas.width/2,0);
-  ctx.lineTo(ctx.canvas.width/2,ctx.canvas.height);
-  ctx.stroke();
-  ctx.lineWidth=0.5;
-  dl(ctx,0,0,ctx.canvas.width,ctx.canvas.height,10);
-  dl(ctx,0,ctx.canvas.height,ctx.canvas.width,0,10);
-  ctx.closePath();
-  }
-//米字格以上 dl qt
-
-  function screencanvas(){
-  	var btnClear=document.getElementById("btnClear");
-  	canvas.width=parseInt(document.body.clientWidth);
-  	canvas.height=parseInt(document.body.offsetHeight)-parseInt(btnClear.offsetHeight)*2-5;
-  	qt(ctx);
+  var screencanvas = function(){
+    var btnClear = document.getElementById("btnClear");
+    canvas.width = parseInt(document.body.clientWidth);
+    canvas.height = parseInt(document.body.offsetHeight) - parseInt(btnClear.offsetHeight) * 2 - 5;
+    qt(ctx);
   }
   window.addEventListener("load",screencanvas,true);
   window.addEventListener("resize",screencanvas,true);
 
-  function clearPrint(){
-    $.x=[];
-    $.y=[];
-    $.time=[];
-    $.speed=[];
-    $.pressure=[];
-    $.count=0;
-    $.distance=[];
-    $.locks=[];
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    qt(ctx);
-  }
-
-  function cloneCharData(obj){
-    var result = {};
-    for(var key in obj){
-      result[key] = obj[key];
+  var parameter = function(){
+    var gaussian = function(v, gauss) {//高斯计算公式
+      return ((1 / (Math.sqrt(2 * Math.PI) * gauss)) * Math.pow(Math.E,-(v * v) / (2 * gauss * gauss)));
     }
-    return result;
-  }
+    var ratio = $.maxPress / gaussian(0,$.gaoss);
+    var timeGap = $.time[$.count - 1] - $.time[$.count - 2];
+    var distance = Math.sqrt(Math.pow($.x[$.count - 1] - $.x[$.count - 2],2) +
+      Math.pow($.y[$.count - 1] - $.y[$.count - 2],2));
+    $.distance.push(distance);
+    if(timeGap == 0){
+      $.speed[$.count - 1] = $.speed[$.count - 2];
+    }else{
+      $.speed[$.count - 1] = distance / timeGap;
+    }
+    if($.count > 2){
+      $.speed[$.count - 1] = $.speed[$.count - 1] * 0.6 + $.speed[$.count - 2] * 0.3 + $.speed[$.count - 3] * 0.1;
+    }
+    var speed = gaussian($.speed[$.count - 1],$.gaoss);
+    $.pressure[$.count - 1] = (ratio * speed);
+    $.pressure[$.count - 1] = Math.max(Math.min($.pressure[$.count - 1],$.maxPress),$.minPress);
+    $.pressure[$.count - 1] = ($.pressure[$.count - 1] + $.pressure[$.count - 2]) / 2;
+  }//
 
   function pushAll(x,y,time,lock){
     //只提供x,y,time,lock自动计算所有参数，并全部压入
@@ -123,7 +101,7 @@ var W = function(){
     }
   }
 
-  function clearCharData(){//只清楚数据，不清楚画板
+  function clearPrint(){
     $.x=[];
     $.y=[];
     $.time=[];
@@ -132,22 +110,55 @@ var W = function(){
     $.count=0;
     $.distance=[];
     $.locks=[];
-  }
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    qt(ctx);
+  }//清除画板，清空对象
+
+  function clearCharData(){
+    $.x = [];
+    $.y = [];
+    $.time = [];
+    $.speed = [];
+    $.pressure = [];
+    $.count = 0;
+    $.distance = [];
+    $.locks = [];
+  }//只清除数据，不清除画板
+
+  function cloneCharData(obj){
+    var result = {};
+    for(var key in obj){
+      result[key] = obj[key];
+    }
+    return result;
+  }//深度复制对象
 
   function nextChar(){
     charDatas.push(cloneCharData($));
     clearPrint();
   }
 
-
+  function setCountChar(vari){
+    currentChar += vari;
+    if(currentChar <= totalchar && currentChar > 0){
+      countChar.nodeValue = "第" + currentChar + "/" + totalchar + "个汉字";
+    }else if(currentChar > totalchar){
+      --currentChar;
+      return alert("字数超过限制");
+    }else if(currentChar <= 0){
+      ++currentChar;
+      return alert("已经是第一个字了!");
+    }
+  }
 
   return {
 
     ctx : ctx,
     pushAll : pushAll,
     clearPrint : clearPrint,
-    nextchar : nextChar
-
+    cloneCharData : cloneCharData,
+    nextchar : nextChar,
+    setCountChar : setCountChar
 
     // function drawPoint(count,d){
     //   // d是charData对象,count是数组索引
@@ -160,6 +171,7 @@ var W = function(){
     // 			ctx.drawImage(image,x-w,y-w,w*2,w*2);
     // 		}
     // },
+
 
     // drawPointAll = function (d){
     //   // d是charData对象，r是数组索引
@@ -184,26 +196,18 @@ var W = function(){
   var btnClear = document.getElementById("btnClear");
   var btnSave = document.getElementById("btnSave");
   var nextchars = document.getElementById("nextchars");
-  var renew = document.getElementById("renew");
+  var prechars = document.getElementById("prechars");
   // 以上四个获取四个按钮  
+
   btnClear.addEventListener("click",W.clearPrint,false);
 
   nextchars.addEventListener("click",function(){
-    //////////////////////////////////
-    //////////////////////////
-    //以下没有写
-    ///////////////////////
-    ////////////////////////////////////
-   currentChar++;
-   if(currentChar<=totalchar){
-     countChar.nodeValue="第"+currentChar+"个汉字";
-   }else if(currentChar>totalchar){
-     countChar.nodeValue="第"+(--currentChar)+"个汉字";
-     return alert("字数超过限制");
-   }
-    var tmpCharData = cloneCharData($);
-    charDatas.push(tmpCharData);
-    clearPrint();
+    W.nextchar();
+    W.setCountChar(1);
+  },false);
+
+  prechars.addEventListener("click",function(){
+    W.setCountChar(-1);
   },false);
 
 })();
