a5b8ace911e25b7c68139ab3980b10e756c31a01
dawei <weiweicat333@gmail.com>
2016-09-04 16:04:43 +0800

    update

--- js/page/lib/base.js
+++ js/page/lib/base.js
@@ -1,24 +1,58 @@
 _sec.pre = function(main) {
 
-	var it = {};
+	var it = {}
+	,	canvas = document.getElementById('canvas')
+	,	ctx = canvas.getContext("2d")
+	,	btnBack = document.getElementById("btnBack")
+	,	setchar = document.getElementById("setChar")
+	,	insertPic = document.getElementById("insertPic")
+	,	picShare = document.getElementById("picShare")
+	,	image = document.createElement("img")
+	,	dataChars = JSON.parse(localStorage.dataChars)
+	;
+
+	var screenCanvas = function() {  
+	  canvas.width = document.documentElement.clientWidth;
+	  canvas.height = document.documentElement.clientHeight - btnBack.offsetHeight - 5;
+	};
 
 	var parseDOM = function() {
-		var canvas = document.createElement("canvas");
-		canvas.id = "writing";
-		// document.body.insertBefore(canvas,main.nextSibling);
-		// main.appendChild(canvas);
-		var ctx = canvas.getContext("2d");
-		canvas.width = document.documentElement.clientWidth;
-  		canvas.height = document.documentElement.clientHeight - btnBack.offsetHeight-5;
-		var image = document.createElement("img");
 		image.src = "img/model.png";
-  		canvas.style.background = 'black';///////////////
+		window.addEventListener("load",screenCanvas,true);
+		window.addEventListener("resize",screenCanvas,true);
+  		// canvas.style.background = 'black';///////////////
 	};
+
+	var drawPointAll = function(d,i) {
+	  var col = 3, row = 5, beishu = 90, widthbeishu = 3, permY = parseInt(i / col), permX = i % col;
+	  var width = canvas.width / col, height = canvas.height / row;
+	  for(var r = 0; r < d.count; r++) {
+	    if(d.locks[r]) {
+	      var sampleNumber = parseInt(d.distance[r] / 0.5);
+	      for(var u = 0; u < sampleNumber; u++) {
+	        var t = u / (sampleNumber - 1);
+	        var x = ((1.0 - t) * d.x[r - 1] + t * d.x[r]) / canvas.width * beishu + permX * width;//x/canvas.with = X / beishu
+	        var y = ((1.0 - t) * d.y[r - 1] + t * d.y[r]) / canvas.height * 1.5 * beishu + permY * height;
+	        var w = ((1.0 - t) * d.pressure[r - 1] * d.width + t * d.pressure[r] * d.width) / widthbeishu;  
+	        ctx.drawImage(image, x - w, y - w, w * 2, w * 2);     console.log(x,y,w);
+	        // ctx.drawImage(image, 10, 100, 20, 30);
+	      }
+	    }
+	  }
+	}
+
+	var writingChar = function() {console.log(dataChars);
+	  for(var i = 0; i < dataChars.length; i++) {
+	    drawPointAll(dataChars[i], i);     
+	  }
+	}
+
 
 	var handler = {
 
 		init : function () {
 			parseDOM();
+			writingChar();
 		}
 
 
--- js/page/lib/pre.js
+++ js/page/lib/pre.js
@@ -4,7 +4,8 @@ var _sec = {
 		'<button  id="btnBack" type="button"   title="返回画布">返回</button>'+
 		'<button  id="setChar" type="button" title="调整汉字">调整</button>'+
 		'<button  id="insertPic" type="button" title="背景图片">插画</button>'+
-		'<button  id="picShare" type="button" title="分享汉字">分享</button>'
+		'<button  id="picShare" type="button" title="分享汉字">分享</button>'+
+		'<canvas  id="canvas"></canvas>'
 		,insertPic : 
 		'<div id="slider" class="swipe" style="visibility:visible;">  '+
 		    '<div class="swipe-wrap">  '+
